23074fa0bea00acd0a75ebcfa819c3ec0847d366
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-04 22:09:19 -0300

    feat (app server): Implement index service app

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require("reflect-metadata");
+const express = require("express");
+const { AppDataSource } = require("./database/data-source");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use("/api", routes);
+
+AppDataSource.initialize()
+  .then(() => console.log("📦 Database ready!"))
+  .catch((err) => console.error(err));
+
+module.exports = app;

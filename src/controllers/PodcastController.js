5c47198c11525c0b27c79e22667745ecb175bcb6
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-04 22:08:18 -0300

    feat (controllers podcastcontroller): Implement controller of podcast

---
+++ src/controllers/PodcastController.js
@@ -0,0 +1,21 @@
+const PodcastService = require("../services/PodcastService");
+const { AppDataSource } = require("../database/data-source");
+const podcastRepo = AppDataSource.getRepository("Podcast");
+const service = new PodcastService(podcastRepo);
+
+module.exports = {
+  create: async (request, response) => {
+    try {
+      const podcast = await service.create(request.body);
+
+      response.status(201).json(podcast);
+    } catch (err) {
+      response.status(500).json({ message: err.message });
+    }
+  },
+  list: async (request, response) => {
+    const podcasts = await service.list();
+
+    response.json(podcasts);
+  },
+};

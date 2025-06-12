593c848c0c7b59a13d9977eae3407ebf9bb8ea56
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-12 20:19:19 -0300

    feat (database): Update entites for database

--- src/database/data-source.js
+++ src/database/data-source.js
@@ -1,12 +1,13 @@
 const { DataSource } = require("typeorm");
 const Podcast = require("../entities/Podcast");
 const Episode = require("../entities/Episode");
+const User = require("../entities/User");
 
 const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/database.sqlite",
   synchronize: true,
-  entities: [Podcast, Episode],
+  entities: [User, Podcast, Episode],
 });
 
 module.exports = { AppDataSource };

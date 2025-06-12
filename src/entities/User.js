96af65b6ff542c0669dd9ae8f0f9693e7d1d380e
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-12 20:18:32 -0300

    feat (entitites user): Implement entity for user

---
+++ src/entities/User.js
@@ -0,0 +1,24 @@
+const { EntitySchema } = require("typeorm");
+
+module.exports = new EntitySchema({
+  name: "User",
+  tableName: "users",
+  columns: {
+    id: { type: "int", primary: true, generated: true },
+    name: { type: "varchar" },
+    email: { type: "varchar", unique: true },
+    createdAt: { type: "datetime", createDate: true },
+  },
+  relations: {
+    podcasts: {
+      target: "Podcast",
+      type: "one-to-many",
+      inverseSide: "createdBy",
+    },
+    episodes: {
+      target: "Episode",
+      type: "one-to-many",
+      inverseSide: "createdBy",
+    },
+  },
+});
